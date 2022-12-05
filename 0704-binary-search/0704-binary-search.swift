2ea48066d399d780630949ed485b41c8d8ce9a6c
alisherbaigazin <alisherbaigazin@gmail.com>
2022-12-05 21:44:21 +0600

    Time: 878 ms (13.14%), Space: 14.6 MB (13.94%) - LeetHub

--- 0704-binary-search/0704-binary-search.swift
+++ 0704-binary-search/0704-binary-search.swift
@@ -1,11 +1,17 @@
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
-        for i in 0..<nums.count  {
-            if nums[i] == target {
-                return i
+        var lhs = 0
+        var rhs = nums.count - 1
+        while lhs <= rhs {
+            var mid = (lhs + rhs) / 2
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] > target {
+                rhs = mid - 1
+            } else {
+                lhs = mid + 1
             }
         }
-
         return -1
     }
 }
