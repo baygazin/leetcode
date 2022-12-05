a36aa3e33d7726e1586546774e8fe0a99556feae
alisherbaigazin <alisherbaigazin@gmail.com>
2022-12-06 00:00:19 +0600

    Time: 24 ms (93.98%), Space: 14.6 MB (12.95%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var lhs = 0
+        var rhs = nums.count - 1
+        var mid = 0
+        while lhs <= rhs {
+            mid = (lhs + rhs) / 2
+            
+            if nums[mid] == target {
+                return mid
+            } else if nums[lhs] > target {
+                return lhs
+            } else if nums[rhs] < target {
+                return rhs + 1
+            }
+            
+            if nums[mid] > target {
+                rhs = mid - 1
+            } else {
+                lhs = mid + 1
+            }
+        }
+        return mid
+    }
+}
