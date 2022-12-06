6ba1a054ceb4d737166a64534f4c30562a42b5d2
alisherbaigazin <alisherbaigazin@gmail.com>
2022-12-06 15:01:27 +0600

    Time: 143 ms (35.67%), Space: 14.5 MB (49.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -1,10 +1,11 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        for i in 0..<nums.count - 1 {
-            for j in (i + 1)..<nums.count {
-                if nums[i] + nums[j] == target {
-                    return [i, j]
-                }
+        var dict: [Int: Int] = [:]
+        for firstIndex in 0..<nums.count {
+            if let secondIndex = dict[target - nums[firstIndex]] {
+                return [firstIndex, secondIndex]
+            } else {
+                dict[nums[firstIndex]] = firstIndex
             }
         }
         return []
