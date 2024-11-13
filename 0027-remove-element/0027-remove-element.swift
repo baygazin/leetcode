2dad962dd34854da5b14df3b70e11fc5f113da8f
Alisher Baygazin <alisherbaigazin@gmail.com>
2024-11-13 22:29:39 +0500

    Time: 0 ms (100.00%), Space: 17.6 MB (25.57%) - LeetHub

--- 0027-remove-element/0027-remove-element.swift
+++ 0027-remove-element/0027-remove-element.swift
@@ -1,15 +1,18 @@
 class Solution {
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        var k = 0
         var i = 0
         
         while i < nums.count {
-            if nums[i] == val {
-                nums.remove(at: i)
+            if nums[i] != val {
+                nums[k] = nums[i]
+                i += 1
+                k += 1
             } else {
                 i += 1
             }
         }
         
-        return nums.count
+        return k
     }
 }
