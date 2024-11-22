76b8a06f909394cfd665a34c7ff4393af56e991a
Alisher Baygazin <alisherbaigazin@gmail.com>
2024-11-22 21:28:01 +0500

    Time: 21 ms (7.69%), Space: 19.7 MB (16.67%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func reverseOnlyLetters(_ s: String) -> String {
+        var chars = Array(s)
+
+        var left = 0
+        var right = chars.count - 1
+    
+        while left < right {
+            if chars[left].isEnglishLetter && chars[right].isEnglishLetter {
+                chars.swapAt(left, right)
+                left += 1
+                right -= 1
+            } else if chars[left].isEnglishLetter {
+            right -= 1
+            } else if chars[right].isEnglishLetter {
+            left += 1
+            } else {
+                left += 1
+                right -= 1
+            }
+        }
+        return String(chars)
+    }
+}
+
+extension Character {
+    
+    var isEnglishLetter: Bool {
+        description.range(of: "^[a-zA-Z]*$", options: .regularExpression) != nil
+    }
+}
