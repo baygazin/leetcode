5b0a5fd8c6e7ed229cb544eade47940e614f6faf
Alisher Baygazin <alisherbaigazin@gmail.com>
2024-12-23 23:57:00 +0500

    Time: 0 ms (100.00%), Space: 17.4 MB (85.53%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.swift
@@ -0,0 +1,40 @@
+class Solution {
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        var l = 0
+        var r = matrix.count - 1
+        
+        while l < r {
+            let m = l + (r - l)/2
+            
+            if matrix[m][0] == target {
+                return true
+            } else if matrix[m][0] > target {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        
+        if (matrix[l][0] > target), l > 0 {
+            l -= 1
+        }
+        
+        var l1 = 0
+        var r1 = matrix[l].count - 1
+        print(matrix[l])
+        print(r)
+        while l1 <= r1 {
+            let m = l1 + (r1 - l1)/2
+            
+            if matrix[l][m] == target {
+                return true
+            } else if matrix[l][m] > target {
+                r1 = m - 1
+            } else {
+                l1 = m + 1
+            }
+        }
+        
+        return false
+    }
+}
